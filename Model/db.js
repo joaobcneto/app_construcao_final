f2643674ee1030153259d9ffef194b0733a9d897
joaobcneto <contato@joaobcneto.com.br>
2013-11-08 07:49:59 -0200

    teste botão fotografar

--- Model/db.js
+++ Model/db.js
@@ -170,7 +170,7 @@ function updateForm(id, descricao, valor, imagem){
     document.itemForm.valor.value = valor;
 	
 	$('#imagem').css('display','block');
-	$('#fotografar').css('display','none');
+	//$('#fotografar').css('display','none');
 	$('#imagem').attr('src', imagem);
 }
 
